c21e3aa6c6129010447e7c1d4a20ed4c4497f161
Anastasiia Omelchenko <anastasya.bgd@gmail.com>
2022-12-11 23:17:57 +0300

    first etaration

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let closeButton = document.querySelector('.popup__close-button');
+let inputName = document.querySelector('.form__input-name');
+let inputActivity = document.querySelector('.form__input-activity')
+let profileName = document.querySelector('.profile__name')
+let profileDescription = document.querySelector('.profile__description')
+let form = document.querySelector('.form')
+
+
+editButton.addEventListener('click', function() {
+    popup.classList.add('popup_opened');
+    inputName.value = profileName.textContent
+    inputActivity.value = profileDescription.textContent
+})
+
+
+closeButton.addEventListener('click', function() {
+    popup.classList.remove('popup_opened');
+})
+
+form.addEventListener('submit', function(evt){
+    evt.preventDefault();
+    profileName.textContent = inputName.value;
+    profileDescription.textContent = inputActivity.value;
+    popup.classList.remove('popup_opened');
+})
+
