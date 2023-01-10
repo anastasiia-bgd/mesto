c3ed0d524f2aecd88a9b1edaf47e7ba74e2ad117
Anastasiia Omelchenko <anastasya.bgd@gmail.com>
2023-01-10 22:48:24 +0300

    Сделан основной функционал

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,39 @@
+const popupList = document.querySelectorAll('.popup');
 let editButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
+let popupEdit = document.querySelector('.popup_type_edit');
 let closeButton = document.querySelector('.popup__close-button');
 let inputName = document.querySelector('.form__input_info_name');
 let inputActivity = document.querySelector('.form__input_info_activity')
 let profileName = document.querySelector('.profile__name')
 let profileDescription = document.querySelector('.profile__description')
 let form = document.querySelector('.form')
+let elementsContainer = document.querySelector('.elements')
+let cardTemplate = document.querySelector('#card-template').content;
+let addButton = document.querySelector('.profile__add-button')
+let popupAdd = document.querySelector('.popup_type_add')
+let popupAddCloseButton = popupAdd.querySelector('.popup__close-button');
+let addCardButton = document.querySelector('.add_card_button')
+let titleCard = document.querySelector('.form__input_info_title')
+let linkCard = document.querySelector('.form__input_info_link')
+let popupAddCard = document.querySelector('.popup_type_add')
+let formAddCard = popupAddCard.querySelector('.form')
+let popupImage = document.querySelector('.popup_type_image')
+let popupImageCaption = popupImage.querySelector('.popup__caption');
+let popupImageOpened = popupImage.querySelector('.popup__image');
+let popupImageCloseButton = popupImage.querySelector('.popup__close-button')
 
+function openPopup(popup) {
+    popup.classList.add('popup_opened'); 
+}
+
+function closePopup(popup) {
+    popup.classList.remove('popup_opened') 
+}
 
 editButton.addEventListener('click', function() {
-    popup.classList.add('popup_opened');
+    openPopup(popupEdit)
     inputName.value = profileName.textContent
     inputActivity.value = profileDescription.textContent
-})
-
-
-closeButton.addEventListener('click', function() {
-    popup.classList.remove('popup_opened');
 })
 
 form.addEventListener('submit', function(evt){
@@ -26,3 +43,59 @@ form.addEventListener('submit', function(evt){
     popup.classList.remove('popup_opened');
 })
 
+addButton.addEventListener('click', function() {
+    openPopup(popupAdd)
+})
+
+function addCard (title, link) {
+    elementsContainer.prepend(createCard(title, link));
+  }
+
+  function openImagePopup(title, link) {
+    openPopup(popupImage);
+    popupImageCaption.textContent = title;
+    popupImageOpened.src = link;
+    popupImageOpened.alt = title;
+  }
+
+popupList.forEach((item) => {
+    item.addEventListener('click', (evt) => {
+      if (evt.target.classList.contains('popup_opened') ||
+      evt.target.classList.contains('popup__close-button')) {
+        closePopup(item);
+      }
+    })
+  })  
+
+function createCard (title, link) {
+ 
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    const cardImage = cardElement.querySelector('.card__image')
+  
+    cardElement.querySelector('.card__title').textContent = title;      
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__image').alt = title;
+  
+    
+    cardElement.querySelector('.card__like-button').addEventListener('click', function(evt) {
+      evt.target.classList.toggle('card__like-button_active');})
+    
+    cardElement.querySelector('.card__trash').addEventListener('click', () => {
+      cardElement.remove();});
+    
+    cardImage.addEventListener('click', function() {
+      openImagePopup(title, link);});
+  
+    return cardElement;
+  }
+  // add default card
+  [...initialCards].reverse().forEach(function(card) {
+    addCard(card.name, card.link);
+  })
+
+  addCardButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    addCard(titleCard.value, linkCard.value)
+    formAddCard.reset();
+    closePopup(popupAddCard);
+  })
